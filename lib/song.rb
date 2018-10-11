04c90a21d210d5136fe2be6cdd2a9425e563d950
barbaradecares <barbaradecares@hotmail.com>
2018-10-11 14:42:10 -0500

    class variables and class methods

---
+++ lib/song.rb
@@ -0,0 +1,73 @@
+require 'pry'
+class Song 
+    attr_accessor :name, :artist, :genre
+    
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @@count +=1
+        
+        @@genres << genre
+        @genre = genre
+         
+
+        
+        @artist = artist
+        
+        @@artists << artist
+    end 
+
+    def name 
+        @name
+    end 
+
+    def genre 
+        @genre
+
+    end 
+
+    def artist
+        @artist
+    end 
+
+    def self.count
+        @@count
+    end
+    
+    def self.artists 
+        @@artists.uniq
+        
+    end 
+
+    def self.genres
+        @@genres.uniq
+    end 
+
+    def self.genre_count 
+        genres_hash = {}
+        @@genres.each do |genre|
+            if genres_hash.key?(genre)
+                genres_hash[genre] += 1
+            else 
+                genres_hash[genre] = 1
+            end 
+        end 
+        genres_hash
+    end 
+
+    def self.artist_count 
+        artist_hash = {}
+        @@artists.each do |artist|
+            if artist_hash.key?(artist)
+                artist_hash[artist] += 1
+            else 
+                artist_hash[artist] = 1
+            end 
+        end 
+        artist_hash
+    end 
+end
